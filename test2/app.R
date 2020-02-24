9672c9c19346f23294ba1f880994c11471738bf4
hk_lian <hk_lian@outlook.com>
2020-02-24 20:58:37 +0000

    tidy data from JHU as timeseries and world distribution

--- test2/app.R
+++ test2/app.R
@@ -1,13 +1,14 @@
 # install.packages("shinythemes")
 # remotes::install_github("GuangchuangYu/nCov2019")
-# remotes::install_github("GuangchuangYu/chinamap")library(dplyr)
+# remotes::install_github("GuangchuangYu/chinamap")
 
+library(dplyr)
 library(tidyverse)
 library(ggplot2)
 library(nCov2019)
 library(shiny)
 
-# load data updated today ----
+# Load data updated today ----
 # source: JHU #
 # load accumulated global cases ny different types
 glob_c <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv")
@@ -17,6 +18,34 @@ glob_r <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/ma
 cov_data <- get_nCov2019(lang = 'en')
 cov_hist <- load_nCov2019(lang = 'en')
 data_glob <- cov_data['global', ]
+max_glob = data_glob[1,2]
+
+# Tidy data ----
+glob_c_l <- glob_c %>%
+    pivot_longer(-(`Province/State`:Long), 
+                 names_to = "Date", 
+                 values_to = "confirmed") %>%
+    mutate(Date = (mdy(Date))) # convert string to date-time
+glob_r_l <- glob_r %>%
+    pivot_longer(-(`Province/State`:Long), 
+                 names_to = "Date", 
+                 values_to = "recovered") %>%
+    mutate(Date = mdy(Date))
+glob_d_l <- glob_d %>%
+    pivot_longer(-(`Province/State`:Long), 
+                 names_to = "Date", 
+                 values_to = "death") %>%
+    mutate(Date = mdy(Date))
+glob_tbl <- glob_c_l %>%
+    left_join(glob_r_l) %>%
+    left_join(glob_d_l) %>%
+    pivot_longer(confirmed:death, 
+                 names_to = "Case", 
+                 values_to = "Count")
+glob_timeseries <- glob_tbl %>% group_by(Date, Case) %>%  
+    summarise(total_count = sum(Count))
+
+
 
 # User interface ----
 ui = navbarPage(
@@ -29,11 +58,11 @@ ui = navbarPage(
                      helpText("World map of coronavirus cases"),
                      selectInput("type_glob", 
                                 label = "Type of cases to display",
-                                choices = c("Confirmed", "Recovered", "Deaths"),
-                                selected = "Confirmed"),
+                                choices = c("confirmed", "recovered", "death"),
+                                selected = "confirmed"),
                      sliderInput("range_glob", 
-                        label = "Number of cases to display in log scale",
-                        min = 0, max = max_glob, value = c(0, log(max_glob)))
+                        label = "Number of cases to display in log10 scale",
+                        min = 0, max = max_glob, value = c(0, log10(max_glob)))
                  ),
                  mainPanel(plotOutput("map_glob")),
                  h4("Trend"),
@@ -41,7 +70,7 @@ ui = navbarPage(
                      tabPanel("Confirmed and Suspected Cases", 
                               plotOutput("trend_glob_c")),
                      tabPanel("Added Cases"),
-                     tabPanel("Death Rate"),
+                     tabPanel("Death and Cure Cases"),
                      id = NULL, selected = NULL, type = c("tabs", "pills"),
                      position = NULL),
                  h4("Data Table"),
@@ -58,23 +87,33 @@ ui = navbarPage(
 server = function(input, output) {
     
 # Tab1: Global Map and Trend
-    # estract today's data
-    glob_c_t <- glob_c[,ncol(glob_c)]
-    test <- glob_c %>% group_by(`Country/Region`)
-    test <- summarise(test, Total_today = test[,ncol(test)])
-    output$table_glob <- DT::renderDataTable(DT::datatable({data_glob_}))
+    output$table_glob <- DT::renderDataTable(DT::datatable({glob_c}))
+    # fillter the tibble based on input
+    glob_distribution <- glob_tbl %>%
+        filter(Date == max(Date)) %>%
+        filter(Case == as.character(input$type_glob)) %>%
+        # filter(count)
+        group_by(`Country/Region`) %>%  
+        summarise(total_count = sum(Count))
     # plot the worldmap
-    mapworld <- borders("world", colour = "gray75", fill="white",
+    mapworld <-
+        glob_distribution %>% 
+        borders("world", colour = "gray75", fill = Count,
                         show.legend = FALSE) #basic map
+    
+    ##
+    output$dateRangeText2 <- renderText({
+        paste("input$range_glob is", 
+              paste(as.character(input$range_glob), collapse = " to ")
+        )
+    })
+    ##
     output$map_glob <- renderPlot({
         ggplot() + mapworld + ylim(-60,90) +
             labs(caption = paste("accessed date:", time(cov_data)))
     })
     output$trend_glob_c <- renderPlot({
-        ggplot(summary[cov_data], aes(as.Date(date, "%m.%d"),
-        as.numeric(confirm))) + geom_col(fill='firebrick') 
-        + theme_minimal(base_size = 14) + xlab(NULL) + ylab(NULL) 
-        + labs(caption = paste("accessed date:", time(cov_data)))
+        ggplot()
     })
     
 }
