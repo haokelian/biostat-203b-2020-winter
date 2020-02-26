d5f8a947b16c776ab02e76cdd6cba2b72c7c84f7
hk_lian <hk_lian@outlook.com>
2020-02-26 08:04:29 +0000

    add Leaflet map

--- test2/app.R
+++ test2/app.R
@@ -1,4 +1,5 @@
-# install.packages("shinythemes")
+# install.packages("rgdal")
+# install.packages("leaflet")
 # remotes::install_github("GuangchuangYu/nCov2019")
 # remotes::install_github("GuangchuangYu/chinamap")
 
@@ -8,6 +9,8 @@ library(ggplot2)
 library(nCov2019)
 library(shiny)
 library(lubridate)
+library(leaflet) 
+library(rgdal)
 
 # Load data updated today ----
 # source: JHU #
@@ -18,8 +21,7 @@ glob_r <- read_csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/ma
 # source: Weixin #
 cov_data <- get_nCov2019(lang = 'en')
 cov_hist <- load_nCov2019(lang = 'en')
-data_glob <- cov_data['global', ]
-max_glob = data_glob[1,2]
+
 
 # Tidy data ----
 glob_c_l <- glob_c %>%
@@ -67,20 +69,15 @@ glob_recovered_number <- glob_tbl %>%
     summarise(recovered_count = sum(Count)) %>%
     filter(Case == "recovered")
 
-glob_deathrate <- glob_confirmed_number %>%
+glob_death_heal <- glob_confirmed_number %>%
     left_join(glob_death_number, by = "Date") %>%
-    mutate(deathrate = 100*death_count/confirmed_count)
-
-glob_healrate <- glob_confirmed_number %>%
-    right_join(glob_recovered_number, by = "Date") %>%
-    mutate(healrate = 100*recovered_count/confirmed_count)
-
-glob_death_heal <- glob_deathrate %>%
-    right_join(glob_healrate, by = "Date") %>%
+    left_join(glob_recovered_number, by = "Date") %>%
+    mutate(healrate = 100*recovered_count/confirmed_count) %>%
+    mutate(deathrate = 100*death_count/confirmed_count) %>%
     pivot_longer(ends_with("rate"), 
-                 names_to = "Case", 
+                 names_to = "CaseRate", 
                  values_to = "Rate") %>%
-    select(Date, ends_with("count"), Case, ends_with("rate"))
+    select(Date, ends_with("count"), ends_with("rate"))
 
 # store the update time of data
 lastdate = glob_timeseries %>% tail(1)
@@ -90,25 +87,27 @@ last_date = lastdate[1] %>% pull(Date)
 
 # User interface ----
 ui = navbarPage(
-        "Cronavirus Telescope",
+        theme = "darkly", "Cronavirus Telescope", 
         
 # Tab1: Global Map and Trend 
+        # tabPanel("Map", leafletOutput("map_glob", height=1000)),
         tabPanel("Homepage", icon=icon("home"),
                  titlePanel("Global Map and Trend"),
                  sidebarPanel(
                      helpText("World map and data table of coronavirus cases"),
                      dateInput('date_glob',
                                label = 'Date to display',
-                               value = Sys.Date()),
+                               value = Sys.Date()-1),
                      selectInput("type_glob", 
                                 label = "Type of cases to display",
                                 choices = c("confirmed", "recovered", "death"),
                                 selected = "confirmed"),
                  ),
-                 mainPanel(plotOutput("map_glob")),
+                 mainPanel(helpText("Cases numbers are shown in log scale"),
+                           leafletOutput("map_glob")),
                  h4("Data Table of Global Cases in Map"),
                  DT::dataTableOutput("table_glob"),
-                 
+                 DT::dataTableOutput("test"),
                  h4("Trend"),
                  tabsetPanel(
                      tabPanel("Global Cases", 
@@ -142,13 +141,39 @@ server = function(input, output) {
         glob_distribution
     }))
     
+    ##
+    output$test <- DT::renderDataTable(DT::datatable({
+        glob_mapdata <- glob_tbl %>%
+            filter(Date == as.character(input$date_glob)) %>%
+            filter(Case == as.character(input$type_glob)) %>%
+            arrange(desc(Count)) 
+        glob_mapdata
+    }))
+    ##
+    
     # plot the worldmap
-    mapworld <-
-        borders("world", colour = "gray75", fill = "White",
-                        show.legend = FALSE) #basic map
-    output$map_glob <- renderPlot({
-        ggplot() + mapworld + ylim(-60,90) +
-            labs(caption = paste("accessed date:", time(cov_data)))
+    ##
+    # output$map_glob <- renderPlot({
+    #     mapworld <-
+    #     borders("world", colour = "gray75", fill = "White",
+    #                     show.legend = FALSE) #basic map
+    #     ggplot() + mapworld + ylim(-60,90) +
+    #         labs(caption = paste("accessed date:", time(cov_data)))
+    # })
+    ##
+    output$map_glob <- renderLeaflet({
+        # glob_mapdata <- glob_tbl %>%
+        #     filter(Date == as.character(input$date_glob)) %>%
+        #     filter(Case == as.character(input$type_glob)) %>%
+        #     arrange(desc(Count)) 
+        Color <- switch(input$type_glob, 
+                       "confirmed" = "#FF7F50",
+                       "recovered" = "#A2CD5A",
+                       "death" = "#8B4513")
+        leaflet(glob_mapdata) %>% addTiles() %>%
+            addCircles(lng = ~Long, lat = ~Lat, weight = 1, color = Color,
+                       radius = ~sqrt(log10(Count)) * 100000,
+                       popup = ~`Province/State`)
     })
     
     # plot the trends
@@ -165,7 +190,7 @@ server = function(input, output) {
     })
     output$trend_glob_r <- renderPlot({
         glob_death_heal %>%
-            ggplot(mapping = aes(x = Date, y = Rate, color = Case)) +
+            ggplot(mapping = aes(x = Date, y = Rate, color = CaseRate)) +
             geom_line() + geom_point() +
             scale_color_manual(values = c("#666666", "#00CD00")) +
             labs(y = "Percents", caption = paste("accessed date:", last_date))
